e5e981ae8bae977723fedefcfffe09e118dfa92e
Matheus Di Donato <matheusdidonato@yahoo.com.br>
2021-02-28 22:58:33 -0300

    feature/implements-login-validation-and-creation

--- src/main/java/com/oficinagenericagestao/oficinagenericagestao/domain/Usuario.java
+++ src/main/java/com/oficinagenericagestao/oficinagenericagestao/domain/Usuario.java
@@ -20,8 +20,10 @@ public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+
+
     private String nome;
     private String cpf;
     private String senha;
